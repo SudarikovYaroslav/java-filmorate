169bd1d62400a6bc181b62b04d13789d97725891
Sudarikov_Iaroslav <yarik846@mail.ru>
2022-05-27 16:34:54 +0300

    feat: добаил lombok в User и Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,8 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
+
 import java.time.Duration;
 import java.time.LocalDate;
 
+@Data
 public class Film {
     private long id;
     private String name;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,7 +1,10 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
 public class User {
     private long id;
     private String email;
