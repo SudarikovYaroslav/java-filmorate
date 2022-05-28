1262c835dc374bec067e8c453b56ab8a1627f648
Sudarikov_Iaroslav <yarik846@mail.ru>
2022-05-28 09:54:47 +0300

    feat: добавлено логирование в контроллеры

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.InvalidFilmException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -10,6 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @RestController
+@Slf4j
 public class FilmController {
     private static final long MAX_DESCRIPTION_LENGTH = 200L;
     private static final LocalDate FIRST_FILM_BIRTHDAY = LocalDate.of(1895, Month.DECEMBER, 28);
@@ -20,34 +22,48 @@ public class FilmController {
     public void addFilm(@RequestBody Film film) throws InvalidFilmException {
         validateFilm(film);
         films.add(film);
+        log.debug("Добавлен фильм: " + film.getName());
     }
 
     @PutMapping
     public void updateFilm(@RequestBody Film film) throws InvalidFilmException {
         validateFilm(film);
         films.add(film);
+        log.debug("Обновлён фильм: " + film.getName());
     }
 
     @GetMapping
     public Set<Film> getFilms() {
+        log.debug("Текущее количество фильмов: " + films.size());
         return films;
     }
 
     private void validateFilm(Film film) throws InvalidFilmException {
-        if (film == null) throw new NullPointerException("Передано пустое значение фильма!");
+        if (film == null) {
+            log.warn("Передано пустое значение фильма");
+            throw new NullPointerException("Передано пустое значение фильма!");
+        }
 
-        if (film.getName() == null || film.getName().isBlank()) throw new InvalidFilmException(
-                "Название фильма не может быть пустым!");
+        if (film.getName() == null || film.getName().isBlank()) {
+            log.warn("Передано пустое название фильма");
+            throw new InvalidFilmException("Название фильма не может быть пустым!");
+        }
 
-        if (film.getDescription() != null && film.getDescription().length() > MAX_DESCRIPTION_LENGTH)
+        if (film.getDescription() != null && film.getDescription().length() > MAX_DESCRIPTION_LENGTH) {
+            log.warn("Передана недопустимая длинна описания фильма");
             throw new InvalidFilmException(
                     "Максимальная длинна описания фильма: " + MAX_DESCRIPTION_LENGTH + " символов!");
+        }
 
-        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(FIRST_FILM_BIRTHDAY))
+        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(FIRST_FILM_BIRTHDAY)) {
+            log.warn("Передана некорректная дата релиза фильма");
             throw new InvalidFilmException(
-                "Дата релиза не может быть раньше чем день рождения кино: " + FIRST_FILM_BIRTHDAY);
+                    "Дата релиза не может быть раньше чем день рождения кино: " + FIRST_FILM_BIRTHDAY);
+        }
 
-        if (film.getDuration() != null && film.getDuration().getSeconds() < 0)
+        if (film.getDuration() != null && film.getDuration().getSeconds() < 0) {
+            log.warn("Передана отрицательная продолжительность фильма");
             throw new InvalidFilmException("Продолжительность фильма должна быть положительной!");
+        }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.InvalidUserException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -10,6 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class UserController {
 
     private final Map<String, User> users = new HashMap<>();
@@ -18,31 +20,46 @@ public class UserController {
     public void createUser(@RequestBody User user) throws InvalidUserException {
         validateUser(user);
         users.put(user.getEmail(), user);
+        log.debug("Добавлен пользователь: " + user.getLogin() + " - email: " + user.getEmail());
     }
 
     @PutMapping
     public void updateUser(@RequestBody User user) throws InvalidUserException {
         validateUser(user);
         users.put(user.getEmail(), user);
+        log.debug("Обновлён пользователь: " + user.getLogin() + " - email: " + user.getEmail());
     }
 
     @GetMapping
     public Collection<User> getUsers() {
+        log.debug("Текущее количество пользователей: " + users.size());
         return users.values();
     }
 
     private void validateUser(User user) throws InvalidUserException {
-        if (user == null) throw new NullPointerException("Передано пустое значение пользователя!");
+        if (user == null) {
+            log.warn("Передано пустое значение пользователя");
+            throw new NullPointerException("Передано пустое значение пользователя!");
+        }
 
-        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@"))
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.warn("Передано невалидное значение email");
             throw new InvalidUserException("Электронная почта не может быть пустой и должна содержать символ @");
+        }
 
-        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" "))
+        if (user.getLogin() == null || user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            log.warn("Передано невалидное значение login");
             throw new InvalidUserException("Логин не может быть пустым и содержать пробелы!");
+        }
 
-        if (user.getName() == null || user.getName().isBlank()) user.setName(user.getLogin());
+        if (user.getName() == null || user.getName().isBlank()) {
+            log.debug("Передано пустое name, присвоено имя name = " + user.getLogin());
+            user.setName(user.getLogin());
+        }
 
-        if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now()))
+        if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Передана некорректная дата рождения");
             throw new InvalidUserException("Дата рождения не может быть в будущем");
+        }
     }
 }
